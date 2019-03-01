07ca77225e47076ce954633589b98abf6e3646b1
Giulia <giulia.coro@ennova-research.com>
2019-03-01 12:40:31 +0100

    Useless commit

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const path = process.argv[2];
 const name = path.split('/')[path.split('/').length - 1].split('.')[0];
 const fs = require('fs');
 
-function readContent() {
+const readContent = () => {
 	return fs.readFileSync(path, 'utf8');
 }
 
-function writeToFile(rows) {
+const writeToFile = (rows) => {
 	fs.writeFileSync('./' + name + '.out', rows.join('\n'), 'utf8')
 }
 
-function parseInput(contentToParse) {
-	console.log('parse input start')
+const parseInput = (contentToParse) => {
 	const lines = contentToParse.split('\n');
 	if (lines[lines.length - 1] === '')
 		lines.splice(lines.length - 1, 1);
@@ -34,13 +33,11 @@ function parseInput(contentToParse) {
 			verticalPics.push({ id, orientation, tagsNumber, tags })
 		}
 	}
-	console.log('parse input end')
-	return {
-		horizontalPics, verticalPics
-	};
+	console.log('finished parsing input')
+	return { horizontalPics, verticalPics };
 }
 
-function parseOutput(results) {
+const parseOutput = (results) => {
 	const number = result.length;
 	const rest = results.map(item => {
 		if (item.orientation === '2V')
@@ -52,44 +49,44 @@ function parseOutput(results) {
 	return [number].concat(rest);
 }
 
-function mergeVerticalPics(verticalPics) {
+const mergeVerticalPics = (verticalPics) => {
 	console.log('mergeVerticalPics start')
 	console.log('abbiamo' + verticalPics.length + 'foto verticali')
 	const bffList = [];
 	while (verticalPics.length > 1) {
 		let pic = verticalPics.splice(0, 1)[0];
-		let bfIndex = undefined;
-		let bfTags = new Set([]);
+		let currentBestMatchIndex = undefined;
+		let currentBestMatchTags = new Set([]);
 		//find best friend
 		for (let i = 0; i < verticalPics.length; i++) {
 			let newBffTags = new Set(verticalPics[i].tags.concat(pic.tags))
-			if (newBffTags.size >bfTags.size) {
-				bfTags = newBffTags;
-				bfIndex = i;
+			if (newBffTags.size > currentBestMatchTags.size) {
+				currentBestMatchTags = newBffTags;
+				currentBestMatchIndex = i;
 			}
 		}
 		const newSlide = {
-			id: [pic.id, verticalPics.splice(bfIndex, 1)[0].id],
+			id: [pic.id, verticalPics.splice(currentBestMatchIndex, 1)[0].id],
 			orientation: '2V',
-			tagsNumber: bfTags.size,
-			tags: bfTags
+			tagsNumber: currentBestMatchTags.size,
+			tags: currentBestMatchTags
 		};
-		//console.log(newSlide)
+		console.log(newSlide)
 		bffList.push(newSlide)
 	}
 	console.log('mergeVerticalPics end')
 	return bffList;
 }
 
-function calculateMatches(item1, item2) {
+const calculateMatches = (item1, item2) => {
 	let matches = 0;
-	if(item2 && item1) {
+	if (item2 && item1) {
 		item1.tagsNumber < item2.tagsNumber ? item1.tags.forEach(item => item2.tags.has(item) ? matches++ : null) : item2.tags.forEach(item => item1.tags.has(item) ? matches++ : null)
 	}
 	return matches
 }
 
-function getBestMatch(initialSlide, remaining) {
+const getBestMatch = (initialSlide, remaining) => {
 	if (remaining.length === 1)
 		return remaining.splice(0, 1)[0];
 	const maxPossible = Math.floor(initialSlide.tags.size / 2)
@@ -98,26 +95,24 @@ function getBestMatch(initialSlide, remaining) {
 		const matches = calculateMatches(initialSlide, remaining[j])
 		const s1only = initialSlide.tagsNumber - matches;
 		const s2only = remaining[j].tagsNumber - matches;
-		const count2 = Math.min(s1only, s2only, matches)
-		if (count2 > max.count) {
-			max = { slide: j, count: count2 };
+		const count = Math.min(s1only, s2only, matches)
+		if (count > max.count) {
+			max = { slide: j, count };
 		}
 	}
 	return remaining.splice(max.slide, 1)[0];
 }
 
-
-
-function getSlideshow(horizontalPics, verticalPics) {
+const getSlideshow = (horizontalPics, verticalPics) => {
 	const doubleVerticalPics = mergeVerticalPics(verticalPics)
 	let allSlides = horizontalPics.concat(doubleVerticalPics)
-	allSlides = allSlides.sort((a,b) => a.tagsNumber - b.tagsNumber)
+	allSlides = allSlides.sort((a, b) => a.tagsNumber - b.tagsNumber)
 	const result = allSlides.splice(0, 1);
 
 	while (allSlides.length > 0) {
 		const theSlide = getBestMatch(result[result.length - 1], allSlides);
 		result.push(theSlide);
-		//console.log(allSlides)
+		console.log(allSlides)
 	}
 	// console.log(result)
 	return result;
